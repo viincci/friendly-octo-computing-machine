fcb0267c21faebe996ea985f4c2936c58c4c12e9
Vinciii <105530911+viincci@users.noreply.github.com>
2025-03-23 13:31:04 +0200

    Update mainbitmexlivetrader.py

--- mainbitmexlivetrader.py
+++ mainbitmexlivetrader.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1ceqPVi26bxYuJdDijAZxhfx8k6f1Nnit
 """
 
-!pip install -q yfinance bitmex matplotlib python-telegram-bot nest_asyncio
+#!pip install -q yfinance bitmex matplotlib python-telegram-bot nest_asyncio
 
 # -*- coding: utf-8 -*-
 import numpy as np
