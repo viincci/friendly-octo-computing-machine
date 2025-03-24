dd1417ae7a492afc3ee6981e36973a49cba4e308
Vinciii <105530911+viincci@users.noreply.github.com>
2025-03-24 03:08:38 +0200

    Update mainbitmexlivetrader.py

--- mainbitmexlivetrader.py
+++ mainbitmexlivetrader.py
@@ -1512,7 +1512,7 @@ class BitMEXLiveTrader:
                 signal = self.check_for_signals(df_analyzed)
                 
                 # Show current balance after trade execution
-                if signal is not None or type(signal) == NoneType:
+                if signal is not None or type(signal) == None:
                     logger.info(f"Results of Checking for signals :\n {signal.head()}")
                     #  signal if any
                     self.execute_signal(signal)
